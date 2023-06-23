b9d3f40fa370056484112fe645dff7a678180cca
Raynald <raynaldkrisnawan@gmail.com>
2023-06-23 17:31:08 +0700

    update database file

--- database/sewakamera_db.sql
+++ database/sewakamera_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 21, 2023 at 11:43 AM
+-- Generation Time: Jun 23, 2023 at 12:29 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,7 +30,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `data_sewa` (
   `id` int(11) NOT NULL,
   `id_history` int(11) NOT NULL,
-  `id_kamera` int(11) NOT NULL,
+  `id_kamera` int(11) DEFAULT NULL,
+  `model` varchar(30) NOT NULL,
+  `merk` varchar(30) NOT NULL,
+  `price` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL,
   `status` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -39,38 +42,41 @@ CREATE TABLE `data_sewa` (
 -- Dumping data for table `data_sewa`
 --
 
-INSERT INTO `data_sewa` (`id`, `id_history`, `id_kamera`, `jumlah`, `status`) VALUES
-(1, 9, 8, 2, 'Belum Dikembalikan'),
-(2, 9, 4, 1, 'Belum Dikembalikan'),
-(3, 10, 4, 3, 'Belum Dikembalikan'),
-(4, 10, 8, 3, 'Belum Dikembalikan'),
-(5, 11, 1, 2, 'Belum Dikembalikan'),
-(6, 11, 4, 3, 'Belum Dikembalikan'),
-(7, 11, 8, 3, 'Belum Dikembalikan'),
-(8, 12, 4, 2, 'Dikembalikan'),
-(9, 12, 8, 3, 'Dikembalikan'),
-(10, 13, 8, 1, 'Dikembalikan'),
-(11, 13, 4, 1, 'Dikembalikan'),
-(12, 14, 4, 1, 'Dikembalikan'),
-(13, 14, 8, 1, 'Dikembalikan'),
-(14, 15, 8, 2, 'Dikembalikan'),
-(15, 15, 4, 1, 'Dikembalikan'),
-(16, 16, 8, 2, 'Dikembalikan'),
-(17, 16, 4, 1, 'Dikembalikan'),
-(18, 16, 1, 3, 'Dikembalikan'),
-(19, 17, 8, 2, 'Dikembalikan'),
-(20, 17, 4, 2, 'Dikembalikan'),
-(21, 17, 1, 3, 'Dikembalikan'),
-(22, 18, 8, 2, 'Dikembalikan'),
-(23, 18, 4, 2, 'Dikembalikan'),
-(24, 18, 2, 1, 'Dikembalikan'),
-(25, 18, 1, 3, 'Dikembalikan'),
-(26, 19, 8, 2, 'Dikembalikan'),
-(27, 19, 4, 2, 'Dikembalikan'),
-(28, 19, 1, 3, 'Dikembalikan'),
-(29, 20, 8, 2, 'Dikembalikan'),
-(30, 20, 4, 3, 'Dikembalikan'),
-(31, 21, 1, 2, 'Dikembalikan');
+INSERT INTO `data_sewa` (`id`, `id_history`, `id_kamera`, `model`, `merk`, `price`, `jumlah`, `status`) VALUES
+(2, 9, 4, '', '', 0, 1, 'Belum Dikembalikan'),
+(3, 10, 4, '', '', 0, 3, 'Belum Dikembalikan'),
+(5, 11, 1, '', '', 0, 2, 'Belum Dikembalikan'),
+(6, 11, 4, '', '', 0, 3, 'Belum Dikembalikan'),
+(8, 12, 4, '', '', 0, 2, 'Dikembalikan'),
+(11, 13, 4, '', '', 0, 1, 'Dikembalikan'),
+(12, 14, 4, '', '', 0, 1, 'Dikembalikan'),
+(15, 15, 4, '', '', 0, 1, 'Dikembalikan'),
+(17, 16, 4, '', '', 0, 1, 'Dikembalikan'),
+(18, 16, 1, '', '', 0, 3, 'Dikembalikan'),
+(20, 17, 4, '', '', 0, 2, 'Dikembalikan'),
+(21, 17, 1, '', '', 0, 3, 'Dikembalikan'),
+(23, 18, 4, '', '', 0, 2, 'Dikembalikan'),
+(24, 18, 2, '', '', 0, 1, 'Dikembalikan'),
+(25, 18, 1, '', '', 0, 3, 'Dikembalikan'),
+(27, 19, 4, '', '', 0, 2, 'Dikembalikan'),
+(28, 19, 1, '', '', 0, 3, 'Dikembalikan'),
+(30, 20, 4, '', '', 0, 3, 'Dikembalikan'),
+(31, 21, 1, '', '', 0, 2, 'Dikembalikan'),
+(34, 24, NULL, '', '', 0, 3, 'Dikembalikan'),
+(35, 25, 1, '', '', 0, 2, 'Dikembalikan'),
+(36, 26, 10, '', '', 0, 3, 'Dikembalikan'),
+(37, 27, 10, '', '', 0, 3, 'Dikembalikan'),
+(38, 28, 10, '', '', 0, 2, 'Dikembalikan'),
+(39, 29, 10, '', '', 0, 1, 'Dikembalikan'),
+(40, 30, 10, '', '', 0, 2, 'Dikembalikan'),
+(41, 31, 10, '', '', 0, 1, 'Dikembalikan'),
+(42, 32, 1, '', '', 0, 1, 'Dikembalikan'),
+(43, 33, 10, 'a', 'a', 20000, 1, 'Dikembalikan'),
+(44, 34, 10, 'a', 'a', 20000, 1, 'Dikembalikan'),
+(45, 35, 10, 'a', 'a', 20000, 1, 'Dikembalikan'),
+(46, 36, 1, 'Sony A7 IV', 'Sony', 40000, 1, 'Dikembalikan'),
+(47, 37, 1, 'Sony A7 IV', 'Sony', 40000, 1, 'Belum DIkembalikan'),
+(48, 37, 10, 'a', 'a', 20000, 1, 'Belum DIkembalikan');
 
 -- --------------------------------------------------------
 
@@ -114,7 +120,23 @@ INSERT INTO `history` (`id`, `name`, `lama_sewa`, `total`, `description`, `tangg
 (18, 'user', 22, 6490000, 'A(2x), Canon EOS R6(2x), Fujifilm X-H2(1x), Sony A7 IV(3x) ', '2023-06-21 16:23:15', '2023-07-13 16:23:15', 2, 'COMPLETED'),
 (19, 'user', 4, 1040000, 'A(2x), Canon EOS R6(2x), Sony A7 IV(3x) ', '2023-06-21 16:24:44', '2023-06-25 16:24:44', 2, 'COMPLETED'),
 (20, 'user', 2, 380000, 'A(2x), Canon EOS R6(3x) ', '2023-06-21 16:27:16', '2023-06-23 16:27:16', 2, 'COMPLETED'),
-(21, 'user', 3, 240000, 'Sony A7 IV(2x) ', '2023-06-21 16:36:02', '2023-06-24 16:36:02', 2, 'COMPLETED');
+(21, 'user', 3, 240000, 'Sony A7 IV(2x) ', '2023-06-21 16:36:02', '2023-06-24 16:36:02', 2, 'COMPLETED'),
+(22, 'user', 1, 40000, 'A(2x) ', '2023-06-21 21:38:48', '2023-06-22 21:38:48', 2, 'COMPLETED'),
+(23, 'user', 1, 40000, 'A(2x) ', '2023-06-21 21:41:21', '2023-06-22 21:41:21', 2, 'COMPLETED'),
+(24, 'user', 2, 132, 'a(3x) ', '2023-06-21 21:42:51', '2023-06-23 21:42:51', 2, 'COMPLETED'),
+(25, 'user', 2, 160000, 'Sony A7 IV(2x) ', '2023-06-21 22:11:32', '2023-06-23 22:11:32', 2, 'COMPLETED'),
+(26, 'user', 2, 120000, 'a(3x) ', '2023-06-22 15:49:17', '2023-06-24 15:49:17', 2, 'COMPLETED'),
+(27, 'tes', 2, 120000, 'a(3x) ', '2023-06-22 16:00:11', '2023-06-24 16:00:11', 3, 'COMPLETED'),
+(28, 'user', 3, 120000, 'a(2x) ', '2023-06-22 21:03:59', '2023-06-25 21:03:59', 2, 'COMPLETED'),
+(29, 'user', 1, 20000, 'a(1x) ', '2023-06-22 21:06:00', '2023-06-23 21:06:00', 2, 'COMPLETED'),
+(30, 'user', 1, 40000, 'a(2x) ', '2023-06-22 21:07:07', '2023-06-23 21:07:07', 2, 'COMPLETED'),
+(31, 'user', 1, 20000, 'a(1x) ', '2023-06-22 21:08:54', '2023-06-23 21:08:54', 2, 'COMPLETED'),
+(32, 'user', 1, 40000, 'Sony A7 IV(1x) ', '2023-06-22 21:10:22', '2023-06-23 21:10:22', 2, 'COMPLETED'),
+(33, 'user', 1, 20000, 'a(1x) ', '2023-06-23 16:46:13', '2023-06-24 16:46:13', 2, 'COMPLETED'),
+(34, 'user', 1, 20000, 'a(1x) ', '2023-06-23 17:01:01', '2023-06-24 17:01:01', 2, 'COMPLETED'),
+(35, 'user', 1, 20000, 'a(1x) ', '2023-06-23 17:03:07', '2023-06-24 17:03:07', 2, 'COMPLETED'),
+(36, 'user', 1, 40000, 'Sony A7 IV(1x) ', '2023-06-23 17:04:09', '2023-06-24 17:04:09', 2, 'COMPLETED'),
+(37, 'user', 1, 60000, 'Sony A7 IV(1x), a(1x) ', '2023-06-23 17:14:57', '2023-06-24 17:14:57', 2, 'INCOMPLETED');
 
 -- --------------------------------------------------------
 
@@ -136,10 +158,10 @@ CREATE TABLE `kamera` (
 --
 
 INSERT INTO `kamera` (`id`, `model`, `merk`, `price`, `stock`, `img`) VALUES
-(1, 'Sony A7 IV', 'Sony', 40000, 3, '/assets/image/IMG_Sony-A7-IV.png'),
+(1, 'Sony A7 IV', 'Sony', 40000, 1, '/assets/image/IMG_Sony-A7-IV.png'),
 (2, 'Fujifilm X-H2', 'Fujifilm', 35000, 1, '/assets/image/Fujifilm-XH2.png'),
-(4, 'Canon EOS R6', 'Canon', 50000, 3, '/assets/image/Canon-EOS-R6 (1).jpg'),
-(8, 'A', 'A', 20000, 3, '');
+(4, 'Canon EOS R6', 'Canon', 50000, 1, '/assets/image/Canon-EOS-R6 (1).jpg'),
+(10, 'a', 'a', 20000, 2, '');
 
 -- --------------------------------------------------------
 
@@ -235,19 +257,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `data_sewa`
 --
 ALTER TABLE `data_sewa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `kamera`
 --
 ALTER TABLE `kamera`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `login`
@@ -270,7 +292,7 @@ ALTER TABLE `user`
 --
 ALTER TABLE `data_sewa`
   ADD CONSTRAINT `sewa-history` FOREIGN KEY (`id_history`) REFERENCES `history` (`id`),
-  ADD CONSTRAINT `sewa-kamera` FOREIGN KEY (`id_kamera`) REFERENCES `kamera` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `sewa-kamera` FOREIGN KEY (`id_kamera`) REFERENCES `kamera` (`id`) ON DELETE SET NULL;
 
 --
 -- Constraints for table `history`
